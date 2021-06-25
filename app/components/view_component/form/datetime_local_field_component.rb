3a4a8141e4594e08b22bfde563064fd6a70d6b46
Hans Lemuet <hans@spone.net>
2021-06-25 16:25:04 +0200

    Add Form::DatetimeLocalFieldComponent

---
+++ app/components/view_component/form/datetime_local_field_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module ViewComponent
+  module Form
+    class DatetimeLocalFieldComponent < FieldComponent
+      self.tag_klass = Tags::DatetimeLocalField
+    end
+  end
+end
