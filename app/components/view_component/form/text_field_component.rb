8b0970bcef30d700f62c4bee4b71b1d95a3232a5
Hans Lemuet <hans@spone.net>
2021-06-25 16:28:19 +0200

    Remove html_class from Form::TextFieldComponent

--- app/components/view_component/form/text_field_component.rb
+++ app/components/view_component/form/text_field_component.rb
@@ -4,10 +4,6 @@ module ViewComponent
   module Form
     class TextFieldComponent < FieldComponent
       self.tag_klass = Tags::TextField
-
-      def html_class
-        class_names("field", "border-error": method_errors?)
-      end
     end
   end
 end
