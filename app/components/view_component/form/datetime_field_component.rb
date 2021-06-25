4ecb1cc9b7aaa6b87a8b350392c862cd3f680c29
Hans Lemuet <hans@spone.net>
2021-06-25 16:24:56 +0200

    Add Form::DatetimeFieldComponent

---
+++ app/components/view_component/form/datetime_field_component.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module ViewComponent
+  module Form
+    class DatetimeFieldComponent < FieldComponent
+      self.tag_klass = Tags::DatetimeField
+    end
+  end
+end
