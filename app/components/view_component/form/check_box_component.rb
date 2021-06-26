f2aa2635f58907216863e5d96a4123148a567e94
Hans Lemuet <hans@spone.net>
2021-06-26 15:38:41 +0200

    Simplify

--- app/components/view_component/form/check_box_component.rb
+++ app/components/view_component/form/check_box_component.rb
@@ -12,35 +12,15 @@ module ViewComponent
         super(form, object_name, method_name, options)
       end
 
-      def call # rubocop:disable Metrics/AbcSize Metrics/MethodLength
-        if options[:label] == false
-          ActionView::Helpers::Tags::CheckBox.new(
-            object_name,
-            method_name,
-            form,
-            checked_value,
-            unchecked_value,
-            options
-          ).render
-        else
-          render(ViewComponent::Form::LabelComponent.new(form, object_name, method_name)) do
-            concat ActionView::Helpers::Tags::CheckBox.new(
-              object_name,
-              method_name,
-              form,
-              checked_value,
-              unchecked_value,
-              options
-            ).render
-            concat tag.span(check_box_text)
-          end
-        end
-      end
-
-      def check_box_text
-        @check_box_text ||= ActionView::Helpers::Tags::Translator.new(
-          object, object_name, method_name, scope: "helpers.checkbox"
-        ).translate
+      def call
+        ActionView::Helpers::Tags::CheckBox.new(
+          object_name,
+          method_name,
+          form,
+          checked_value,
+          unchecked_value,
+          options
+        ).render
       end
     end
   end
