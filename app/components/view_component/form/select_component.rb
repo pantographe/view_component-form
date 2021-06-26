c2592d53c21750dc6db24f77f19f4411ae7d7f7d
Hans Lemuet <hans@spone.net>
2021-06-26 17:16:53 +0200

    Add missing namespace

--- app/components/view_component/form/select_component.rb
+++ app/components/view_component/form/select_component.rb
@@ -1,26 +1,28 @@
 # frozen_string_literal: true
 
-module Form
-  class SelectComponent < FieldComponent
-    attr_reader :choices, :html_options
+module ViewComponent
+  module Form
+    class SelectComponent < FieldComponent
+      attr_reader :choices, :html_options
 
-    def initialize(form, object_name, method_name, choices = nil, options = {}, html_options = {}) # rubocop:disable Metrics/ParameterLists
-      @choices = choices
-      @html_options = html_options
+      def initialize(form, object_name, method_name, choices = nil, options = {}, html_options = {}) # rubocop:disable Metrics/ParameterLists
+        @choices = choices
+        @html_options = html_options
 
-      super(form, object_name, method_name, options)
+        super(form, object_name, method_name, options)
 
-      set_html_options!
-    end
+        set_html_options!
+      end
 
-    def call
-      ActionView::Helpers::Tags::Select.new(object_name, method_name, form, choices, options, html_options, &content).render
-    end
+      def call
+        ActionView::Helpers::Tags::Select.new(object_name, method_name, form, choices, options, html_options, &content).render
+      end
 
-    protected
+      protected
 
-    def set_html_options!
-      @html_options[:class] = class_names(html_options[:class], html_class)
+      def set_html_options!
+        @html_options[:class] = class_names(html_options[:class], html_class)
+      end
     end
   end
 end
