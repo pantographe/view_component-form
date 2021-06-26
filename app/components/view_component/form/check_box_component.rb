2264f9d8db068d0b37e5c67a8dd28807238046d1
Hans Lemuet <hans@spone.net>
2021-06-26 14:51:56 +0200

    Add Form::CheckBoxComponent

---
+++ app/components/view_component/form/check_box_component.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+module ViewComponent
+  module Form
+    class CheckBoxComponent < FieldComponent
+      attr_reader :checked_value, :unchecked_value
+
+      def initialize(form, object_name, method_name, checked_value, unchecked_value, options = {}) # rubocop:disable Metrics/ParameterLists
+        @checked_value   = checked_value
+        @unchecked_value = unchecked_value
+
+        super(form, object_name, method_name, options)
+      end
+
+      def call # rubocop:disable Metrics/AbcSize
+        if options[:label] == false
+          ActionView::Helpers::Tags::CheckBox.new(object_name, method_name, form, checked_value, unchecked_value, options).render
+        else
+          render(ViewComponent::Form::LabelComponent.new(form, object_name, method_name, class: "form-checkbox")) do
+            concat ActionView::Helpers::Tags::CheckBox.new(
+              object_name,
+              method_name,
+              form,
+              checked_value,
+              unchecked_value,
+              options
+            ).render
+            concat tag.span(check_box_text, class: "form-checkbox__label")
+          end
+        end
+      end
+
+      def check_box_text
+        @check_box_text ||= ActionView::Helpers::Tags::Translator.new(
+          object, object_name, method_name, scope: "helpers.checkbox"
+        ).translate
+      end
+    end
+  end
+end
