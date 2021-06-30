178147fd03bb78068926bacdc1c3dbb0a80a1366
Hans Lemuet <hans@spone.net>
2021-06-30 12:15:35 +0200

    Add tests

---
+++ spec/view_component/form/text_area_component_spec.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+RSpec.describe ViewComponent::Form::TextAreaComponent, type: :component do
+  let(:object)  { OpenStruct.new }
+  let(:form)    { form_with(object) }
+  let(:options) { {} }
+
+  let(:component) { render_inline(described_class.new(form, object_name, :bio, options)) }
+  let(:component_html_attributes) { component.css("textarea").first.attributes }
+
+  context "with simple args" do
+    it do
+      expect(component.to_html).to eq(
+        %(<textarea name="user[bio]" id="user_bio">\n</textarea>)
+      )
+    end
+  end
+
+  include_examples "component with custom html classes"
+  include_examples "component with custom data attributes"
+end
