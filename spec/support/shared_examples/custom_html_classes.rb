16a015205645ca19fa1b7a0b24bd21a3fe4183ca
Hans Lemuet <hans@spone.net>
2021-07-06 13:31:12 +0200

    Refactor shared examples with param

--- spec/support/shared_examples/custom_html_classes.rb
+++ spec/support/shared_examples/custom_html_classes.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-RSpec.shared_examples "component with custom html classes" do
+RSpec.shared_examples "component with custom html classes" do |options_keyword_arg_name = :options|
   context "with custom html classes" do
-    let(:options) { { class: "custom-css-class" } }
+    let(options_keyword_arg_name) { { class: "custom-css-class" } }
 
     it { expect(component_html_attributes["class"].to_s).to include("custom-css-class") }
   end
