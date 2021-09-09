99967b002998f7af5c4102d0c012dbc57e12d0fc
Hans Lemuet <hans@spone.net>
2021-09-09 21:46:46 +0200

    Fix LabelComponent block specs

--- spec/view_component/form/label_component_spec.rb
+++ spec/view_component/form/label_component_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 RSpec.describe ViewComponent::Form::LabelComponent, type: :component do
-  let(:object)  { OpenStruct.new }
+  let(:object)  { OpenStruct.new(errors: []) }
   let(:form)    { form_with(object) }
   let(:options) { {} }
   let(:block)   { nil }
@@ -44,8 +44,8 @@ RSpec.describe ViewComponent::Form::LabelComponent, type: :component do
 
   context "with a block and translation param" do
     let(:block) do
-      proc do |translation|
-        "<span class=\"translated-label\">#{translation}</span>".html_safe
+      proc do |component|
+        "<span class=\"translated-label\">#{component.translation}</span>".html_safe
       end
     end
 
@@ -58,15 +58,15 @@ RSpec.describe ViewComponent::Form::LabelComponent, type: :component do
 
   context "with a block and builder param" do
     let(:block) do
-      proc do |builder|
-        "<span class=\"translated-label #{"has-error" if builder.object.errors.include?(:first_name)}\">" \
-        "#{builder.translation}</span>".html_safe
+      proc do |component|
+        "<span class=\"translated-label#{" has-error" if component.builder.object.errors.include?(:first_name)}\">" \
+        "#{component.builder.translation}</span>".html_safe
       end
     end
 
     it do
       expect(component.to_html).to eq(
-        %(<label for="user_first_name"><span class="translated-label has-error">First name</span></label>)
+        %(<label for="user_first_name"><span class="translated-label">First name</span></label>)
       )
     end
   end
