e32958b7f5649f3d8db2f14c637786a68abf2334
Hans Lemuet <hans@spone.net>
2021-09-16 21:59:07 +0200

    Use rspec-html-matchers in DatetimeSelectComponent spec

--- spec/view_component/form/datetime_select_component_spec.rb
+++ spec/view_component/form/datetime_select_component_spec.rb
@@ -10,10 +10,24 @@ RSpec.describe ViewComponent::Form::DatetimeSelectComponent, type: :component do
   let(:component_html_attributes) { component.css("select").first.attributes }
 
   context "with simple args" do
-    it do
-      expect(component.to_html).to start_with(
-        %(<select id="user_created_at_1i" name="user[created_at(1i)]">)
-      )
+    it "has a select for the year" do
+      expect(component.to_html).to have_tag("select", with: { id: "user_created_at_1i", name: "user[created_at(1i)]" })
+    end
+
+    it "has a select for the month" do
+      expect(component.to_html).to have_tag("select", with: { id: "user_created_at_2i", name: "user[created_at(2i)]" })
+    end
+
+    it "has a select for the day" do
+      expect(component.to_html).to have_tag("select", with: { id: "user_created_at_3i", name: "user[created_at(3i)]" })
+    end
+
+    it "has a select for the hours" do
+      expect(component.to_html).to have_tag("select", with: { id: "user_created_at_4i", name: "user[created_at(4i)]" })
+    end
+
+    it "has a select for the minutes" do
+      expect(component.to_html).to have_tag("select", with: { id: "user_created_at_5i", name: "user[created_at(5i)]" })
     end
   end
 
