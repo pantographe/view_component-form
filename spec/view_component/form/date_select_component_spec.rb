c5814ad4a563817a4561f6891d674176704c3327
Hans Lemuet <hans@spone.net>
2021-09-16 21:45:17 +0200

    Use rspec-html-matchers in DateSelectComponent spec

--- spec/view_component/form/date_select_component_spec.rb
+++ spec/view_component/form/date_select_component_spec.rb
@@ -10,10 +10,16 @@ RSpec.describe ViewComponent::Form::DateSelectComponent, type: :component do
   let(:component_html_attributes) { component.css("select").first.attributes }
 
   context "with simple args" do
-    it do
-      expect(component.to_html).to start_with(
-        %(<select id="user_birth_date_1i" name="user[birth_date(1i)]">)
-      )
+    it "has a select for the year" do
+      expect(component.to_html).to have_tag("select", with: { id: "user_birth_date_1i", name: "user[birth_date(1i)]" })
+    end
+
+    it "has a select for the month" do
+      expect(component.to_html).to have_tag("select", with: { id: "user_birth_date_2i", name: "user[birth_date(2i)]" })
+    end
+
+    it "has a select for the day" do
+      expect(component.to_html).to have_tag("select", with: { id: "user_birth_date_3i", name: "user[birth_date(3i)]" })
     end
   end
 
