c9094178620513462c94916682f2357aa7505a9e
Simon Fish <si@mon.fish>
2021-10-06 11:53:39 +0100

    Extract out remove_tag_suffix

--- lib/view_component/form/component_mapping.rb
+++ lib/view_component/form/component_mapping.rb
@@ -8,11 +8,17 @@ module ViewComponent
       end
 
       def method_missing(method_name, _args = nil, *_aargs, **_kwargs)
-        @mapping[method_name.to_s.delete_suffix("_tag")]
+        @mapping.fetch(remove_tag_suffix(method_name))
       end
 
       def respond_to_missing?(method_name, include_private = false)
-        @mapping.key?(method_name.to_s.delete_suffix("_tag")) || super
+        @mapping.key?(remove_tag_suffix(method_name)) || super
+      end
+
+      private
+
+      def remove_tag_suffix(method_name)
+        method_name.to_s.delete_suffix("_tag")
       end
     end
   end
