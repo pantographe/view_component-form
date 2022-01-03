e9468a5a82b76db81f3940bbb6f0e7893b847266
Tobias Ludwig Maier <me@tobiasmaier.info>
2022-01-03 23:34:40 +0100

    Add ErrorMessageComponent and HintComponent
    Based on https://gist.github.com/tmaier/22966c6bddac86e3612c8eddc072b919
    
    Notable differences:
    - self.tag to override the tag used easily. I am not using tag_class, as this is not a classical form component
    - ErrorMessageComponent#messages is public so that one can override it, if necessary

---
+++ app/components/view_component/form/hint_component.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+module ViewComponent
+  module Form
+    class HintComponent < FieldComponent
+      class_attribute :tag, instance_reader: false, instance_writer: false, instance_accessor: false,
+                            instance_predicate: false
+      attr_reader :attribute_content
+
+      self.tag = :div
+
+      def initialize(form, object_name, method_name, content_or_options = nil, options = nil)
+        options ||= {}
+
+        content_is_options = content_or_options.is_a?(Hash)
+        if content_is_options
+          options.merge! content_or_options
+          @attribute_content = nil
+        else
+          @attribute_content = content_or_options
+        end
+
+        super(form, object_name, method_name, options)
+      end
+
+      def call
+        content_or_options = nil
+
+        content_or_options = content || attribute_content if content.present? || attribute_content.present?
+
+        tag.public_send(self.class.tag, messages, options)
+      end
+
+      def render?
+        content.present? || attribute_content.present?
+      end
+    end
+  end
+end
