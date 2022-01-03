542fe79d002bb308d30371f6c57e069d2b4fb470
Tobias Ludwig Maier <me@tobiasmaier.info>
2022-01-03 23:58:59 +0100

    Add Builder#error_message and Builder#hint

--- lib/view_component/form/builder.rb
+++ lib/view_component/form/builder.rb
@@ -189,6 +189,14 @@ module ViewComponent
         end
       end
 
+      def error_message(method, options = {})
+        render_component(:error_message, @object_name, method, objectify_options(options))
+      end
+
+      def hint(method, text = nil, options = {}, &block)
+        render_component(:hint, @object_name, method, text, objectify_options(options), &block)
+      end
+
       private
 
       def render_component(component_name, *args, &block)
