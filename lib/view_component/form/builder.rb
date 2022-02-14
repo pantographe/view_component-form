e88d6be4818e6be4b05e108a1a99e45ee7afc3ea
Hans Lemuet <hans@spone.net>
2022-02-14 22:49:48 +0100

    Backport field_id from Rails 7

--- lib/view_component/form/builder.rb
+++ lib/view_component/form/builder.rb
@@ -197,6 +197,25 @@ module ViewComponent
         render_component(:hint, @object_name, method, text, objectify_options(options), &block)
       end
 
+      # Backport field_id from Rails 7.0
+      if Rails::VERSION::MAJOR < 7
+        def field_id(method_name, *suffixes, namespace: @options[:namespace], index: @index)
+          object_name = object_name.model_name.singular if object_name.respond_to?(:model_name)
+
+          sanitized_object_name = object_name.to_s.gsub(/\]\[|[^-a-zA-Z0-9:.]/, "_").delete_suffix("_")
+
+          sanitized_method_name = method_name.to_s.delete_suffix("?")
+
+          [
+            namespace,
+            sanitized_object_name.presence,
+            (index unless sanitized_object_name.empty?),
+            sanitized_method_name,
+            *suffixes
+          ].tap(&:compact!).join("_")
+        end
+      end
+
       private
 
       def render_component(component_name, *args, &block)
