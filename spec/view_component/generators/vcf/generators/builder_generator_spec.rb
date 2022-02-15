5f3178aec5860688176564e4da577291761bdd57
Hans Lemuet <hans@spone.net>
2022-02-15 01:21:27 +0100

    Add specs for params

--- spec/view_component/generators/vcf/generators/builder_generator_spec.rb
+++ spec/view_component/generators/vcf/generators/builder_generator_spec.rb
@@ -4,9 +4,11 @@ require "generators/vcf/builder/builder_generator"
 RSpec.describe Vcf::Generators::BuilderGenerator, type: :generator do
   destination File.expand_path("../../../../../tmp", __dir__)
 
+  let(:generator) { %w[CustomFormBuilder] }
+
   before do
     prepare_destination
-    run_generator %w[CustomFormBuilder]
+    run_generator generator
   end
 
   describe "the builder" do
@@ -15,5 +17,19 @@ RSpec.describe Vcf::Generators::BuilderGenerator, type: :generator do
     it { is_expected.to exist }
     it { is_expected.to contain(/class CustomFormBuilder < ViewComponent::Form::Builder/) }
     it { is_expected.to contain(/namespace "Form"/) }
+
+    context "with namespace option" do
+      let(:generator) { %w[CustomFormBuilder --namespace=CustomForm] }
+
+      it { is_expected.to contain(/namespace "CustomForm"/) }
+    end
+
+    context "with path option" do
+      subject { file("custom/lib/custom_form_builder.rb") }
+
+      let(:generator) { %w[CustomFormBuilder --path=custom/lib] }
+
+      it { is_expected.to exist }
+    end
   end
 end
