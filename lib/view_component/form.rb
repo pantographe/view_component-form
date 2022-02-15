166cb062b91635ef9a492a8aabe943a2aff08b74
Hans Lemuet <hans@spone.net>
2022-02-15 02:00:16 +0100

    Try using Zeitwerk for loading the gem

--- lib/view_component/form.rb
+++ lib/view_component/form.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
-require_relative "form/version"
-require_relative "form/engine"
-require_relative "form/builder"
+require "view_component"
+
+require "zeitwerk"
+loader = Zeitwerk::Loader.for_gem
+form = "#{__dir__}/form.rb"
+loader.ignore(form)
+loader.push_dir("#{__dir__}/form", namespace: ViewComponent::Form)
+loader.push_dir("#{__dir__}/../../app/components")
+loader.setup
