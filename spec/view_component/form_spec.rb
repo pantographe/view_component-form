355531d8d80e3fc30fb6d373bfaea0cb3829e13b
Hans Lemuet <hans@spone.net>
2022-03-21 10:00:52 +0100

    Add spec to check if ActionText is loaded

--- spec/view_component/form_spec.rb
+++ spec/view_component/form_spec.rb
@@ -4,4 +4,14 @@ RSpec.describe ViewComponent::Form do
   it "has a version number" do
     expect(ViewComponent::Form::VERSION).not_to be_nil
   end
+
+  if ENV.fetch("VIEW_COMPONENT_FORM_USE_ACTIONTEXT", "false") == "true"
+    it "loads ActionText" do
+      expect(ActionView::Helpers::Tags::ActionText).to be_a(Module)
+    end
+  else
+    it "does not load ActionText" do
+      expect(ActionView::Helpers::Tags::ActionText).to be_nil
+    end
+  end
 end
