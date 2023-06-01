bde4926dc4f0c3603235ce83f77de6850982a9c3
Daniel Harrington <daniel.harrington@mindmatters.de>
2023-06-01 13:36:47 +0200

    Fix autoload error
    Zeitwerk::Error:
      loader
    
    <some Zeitwerk::Loader> wants to manage directory
    app/components/concerns, which is already managed by
    <other Zeitwerk::Loader>

--- lib/view_component/form/engine.rb
+++ lib/view_component/form/engine.rb
@@ -6,6 +6,7 @@ module ViewComponent
     class Engine < ::Rails::Engine
       config.autoload_once_paths = %W[
         #{root}/app/components
+        #{root}/app/components/concerns
         #{root}/app/lib
       ]
     end
