4170cfcd5507ac20e27b59032fcb58fac8e1b70c
Daniel Harrington <daniel.harrington@mindmatters.de>
2025-03-18 12:26:03 +0100

    Fix eager loading (Rich)TextAreas in Rails 8
    Rails 8 now support both `text_area` and `textarea` as well as
    `rich_text_area` and `rich_textarea` so we actually need both
    VCF components for both variants to exist.
    
    Original error for undefined `RichTextAreaComponent`:
    
    ```
    .../gems/zeitwerk-2.7.2/lib/zeitwerk/loader/callbacks.rb:32:in
      'Zeitwerk::Loader::Callbacks#on_file_autoloaded':
      expected file .../gems/view_component-form-0.2.10/app/components/view_component/form/rich_text_area_component.rb
      to define constant ViewComponent::Form::RichTextAreaComponent, but didn't (Zeitwerk::NameError)
    ```
    
    Original error for undefined `TextAreaComponent`:
    
    .../gems/ruby-3.4.2/gems/zeitwerk-2.7.2/lib/zeitwerk/loader/callbacks.rb:32:in
      'Zeitwerk::Loader::Callbacks#on_file_autoloaded':
      expected file .../gems/view_component-form-0.2.10/app/components/view_component/form/text_area_component.rb
      to define constant ViewComponent::Form::TextAreaComponent, but didn't (Zeitwerk::NameError)
    ```

--- app/components/view_component/form/rich_text_area_component.rb
+++ app/components/view_component/form/rich_text_area_component.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-if Gem::Version.new(Rails::VERSION::STRING) < Gem::Version.new("8.0")
-  module ViewComponent
-    module Form
-      class RichTextAreaComponent < RichTextareaComponent
-      end
+module ViewComponent
+  module Form
+    class RichTextAreaComponent < RichTextareaComponent
     end
   end
 end
--- app/components/view_component/form/text_area_component.rb
+++ app/components/view_component/form/text_area_component.rb
@@ -1,10 +1,8 @@
 # frozen_string_literal: true
 
-if Gem::Version.new(Rails::VERSION::STRING) < Gem::Version.new("8.0")
-  module ViewComponent
-    module Form
-      class TextAreaComponent < TextareaComponent
-      end
+module ViewComponent
+  module Form
+    class TextAreaComponent < TextareaComponent
     end
   end
 end
