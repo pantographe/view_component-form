a9b1a395b67b357b527f150cd704953f612f288f
Nicolas Brousse <n.brousse@pantographe.studio>
2026-09-25 11:23:09 +0200

    Make rich_textarea accepts block

--- spec/view_component/form/rich_text_area_component_spec.rb
+++ spec/view_component/form/rich_text_area_component_spec.rb
@@ -5,20 +5,37 @@ if defined?(ActionView::Helpers::Tags::ActionText)
     let(:object)  { OpenStruct.new }
     let(:form)    { form_with(object) }
     let(:options) { {} }
+    let(:block)   { nil }
 
-    let(:component) { render_inline(described_class.new(form, object_name, :bio, options)) }
+    let(:component) { render_inline(described_class.new(form, object_name, :bio, options), &block) }
     let(:component_html_attributes) { component.css("trix-editor").first.attributes }
 
     context "with simple args" do
       it "has a hidden field", :aggregate_failures do
-        expect(component.to_html).to have_tag("input",
-                                              with: { type: "hidden", id: "trix_input_1", name: "user[bio]" })
-        expect(component.to_html).to have_tag("trix-editor",
-                                              with: { id: "user_bio", input: "trix_input_1", class: "trix-content" })
+        expect(component.to_html)
+          .to have_tag("input", with: { type: "hidden", id: "trix_input_1", name: "user[bio]" })
+        expect(component.to_html)
+          .to have_tag("trix-editor", with: { id: "user_bio", input: "trix_input_1", class: "trix-content" })
       end
     end
 
     it_behaves_like "component with custom html classes"
     it_behaves_like "component with custom data attributes"
+
+    context "with a block" do
+      let(:block) do
+        proc do
+          "Your <strong>first</strong> text".html_safe
+        end
+      end
+
+      it do
+        expect(component.to_html)
+          .to have_tag("input", with: {
+                         type: "hidden", name: "user[bio]", id: "trix_input_4",
+                         value: "Your <strong>first</strong> text", autocomplete: "off"
+                       })
+      end
+    end
   end
 end
